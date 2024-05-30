34bd6e5c2441317275b9d7f677401d662fbfe4f9
Getinet Bogale <getinet12183@gmail.com>
2024-05-29 23:05:31 -0400

    Normalize line endings

--- project/features/environment.py
+++ project/features/environment.py
@@ -1,9 +1,6 @@
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.by import By
 from selenium import webdriver
 
 from app.application import Application
--- project/features/pages/base_page.py
+++ project/features/pages/base_page.py
@@ -2,7 +2,6 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 from support.logger import logger
-from app import application
 
 
 class BasePage:
